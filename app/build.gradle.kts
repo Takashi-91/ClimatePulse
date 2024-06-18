b6ee30af85178d7e05ce14eedc7a09f8b0894b07
Madimetja Terence Chuene <155132652+Takashi-91@users.noreply.github.com>
2024-06-18 20:34:08 +0000

    Fixing dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,13 +44,21 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     //
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
-    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.3")
-    implementation ("com.squareup.okhttp3:okhttp:4.9.3")  // You can use the latest version available implementation ("com.squareup.okhttp3:logging-interceptor:4.9.3")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3") // For coroutines
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")  // You can use the latest version available implementation ("com.squareup.okhttp3:logging-interceptor:4.9.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3") // For coroutines
+
+        implementation("com.squareup.retrofit2:retrofit:2.9.0")
+        implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+        implementation ("com.squareup.okhttp3:logging-interceptor:4.9.3")
+        implementation ("com.google.android.gms:play-services-location:21.3.0")
+        implementation ("com.github.bumptech.glide:glide:4.12.0")
+        implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
+        implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+        implementation ("com.airbnb.android:lottie:4.2.2")
+    }
 
 
-
-}
